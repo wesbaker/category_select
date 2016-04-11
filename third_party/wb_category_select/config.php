24b3402d58cfd40ff193c1760b802de29643bc36
Peter J. Herrel <peterherrel@gmail.com>
2016-04-11 18:27:03 +0200

    bump version to 1.6

--- third_party/wb_category_select/config.php
+++ third_party/wb_category_select/config.php
@@ -3,7 +3,7 @@
 if (! defined('WB_CAT_SELECT_NAME'))
 {
 	define('WB_CAT_SELECT_NAME', 'WB Category Select');
-	define('WB_CAT_SELECT_VER',  '1.5');
+	define('WB_CAT_SELECT_VER',  '1.6');
 }
 
 $config['name']    = WB_CAT_SELECT_NAME;
